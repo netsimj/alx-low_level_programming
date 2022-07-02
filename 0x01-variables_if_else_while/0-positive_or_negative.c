8559ac97a6e6ed803ab82e601276f05e0acf758e
netsimj <netsimj@gmail.com>
2022-07-01 17:04:29 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,27 +7,21 @@
  * Return: 0
  */
 int main(void)
-<<<<<<< HEAD
-{	int n;
-	int last;
-
-       
-=======
 {
-	int n;
+	int n;i
 	int last;
 
->>>>>>> fc1ae6d46860cda2a83f25f34299df3ce8a7a6c8
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last = n % 10;
 
 	if (last > 5)
-		printf("Last digit of %i is %i and is greater than 5\n", n, last);
+		printf("Last digit of %i is %i and is greater than 5\n", n, las;
 	else if (last == 0)
 		printf("Last digit of %i is %i and is 0\n", n, last);
 	else if (last < 6)
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
-
-	return (0);
+               
+		return (0);
 }
+
