cbe4d2cbf385759d69685bf73a2a1932d4d583d2
netsimj <netsimj@gmail.com>
2022-07-05 14:49:33 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
-
 /** main - prints putchar 
- * Return; always 0
-   */
- * int main(void)
- * {
+* Return; always 0
+*/
+int main(void)
+{
        _putchar('_');
        _putchar('p');
        _putchar('u');
@@ -17,24 +16,5 @@
        _putchar('\n');
 
       return (0);
-=======
-/**
- * main - Print "Holberton"
- *
- * Return: Always 0 (Success)
- */
 
-int main(void)
-{
-	int i = 0;
-	char name_of_school[] = "main";
-
-	while (i < 9)
-	{
-		_putchar(name_of_school[i]);
-		i++;
-	}
-	_putchar('\n');
-	return (0);
 }
->>>>>>> e79bf3d2f1e19637c4398c1e7df865ef01218974
